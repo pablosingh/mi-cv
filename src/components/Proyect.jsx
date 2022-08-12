303ed85db38f1a128abf02585bd7a159e66f8135
Singh Pablo <pablo22_4@hotmail.com>
2022-08-11 23:06:01 -0300

    fix responsive of proyect

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -1,12 +1,14 @@
 import s from '../styles/Proyect.module.css';
+import styled from 'styled-components';
 
 export const Proyect = (props) => {
     const { img, title, description, deploy, repo } = props;
     return (
-        <div className={s.container}>
-            <img src={img} alt='Imagen' className={s.imgProyect}/>
+        <Container>
+            <img src={img} alt='Imagen' className={`image`}/>
             <div>
                 <h2>{title}</h2>
+                <img src={img} alt='Imagen' className={`imagePhone`}/>
                 <p>{description}</p>
                 <div className={s.twoBtn}>
                     <button onClick={ () => { 
@@ -21,6 +23,34 @@ export const Proyect = (props) => {
                     >Repo</button>
                 </div>
             </div>
-        </div>
+        </Container>
     )
 };
+
+const Container = styled.div`
+    width: 85%;
+    margin: 0.3em;
+    padding: 0.5em;
+    border: 1px solid black;
+    border-radius: 1em;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .image{
+        width: 20vw;
+        border-radius: 1em;
+        margin: 0 0.5em;
+        @media(max-width: 768px){
+            display: none;
+        }
+    }
+    .imagePhone{
+        width: 40vw;
+        border-radius: 1em;
+        margin: 0 0.5em;
+        padding: 0.5em 0.5em;
+        @media(min-width: 768px){
+            display: none;
+        }
+    }
+`;
