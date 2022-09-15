c0357d9663643d51e39ef36761853f9f2bc11578
Singh Pablo <pablo22_4@hotmail.com>
2022-09-15 10:59:21 -0300

    divs animados

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,3 +1,4 @@
+
 import { About } from './About';
 import { Name } from './Name';
 import { Contact } from './Contact';
@@ -5,16 +6,32 @@ import { Proyects } from './Proyects';
 import { Skills } from './Skills';
 import { NavBarPhone } from './NavBarPhone';
 import s from '../styles/Body.module.css';
+import DivAnimated from './DivAnimated';
+import styled from "styled-components";
 
 export const Body = () => {
     return (
         <div className={s.containerBody}>
-            <NavBarPhone />
+            {/* <NavBarPhone />
             <Name/>
             <About/>
             <Skills/>
             <Proyects/>
-            <Contact/>
+            <Contact/> */}
+            <NavBarPhone />
+            <Box id='name'><DivAnimated element={ <Name/> }/></Box>
+            <Box id='about'><DivAnimated element={ <About/> }/></Box>
+            <Box id='skills'><DivAnimated element={ <Skills/> }/></Box>
+            <Box id='proyects'><DivAnimated element={ <Proyects/> }/></Box>
+            <Box id='contact'><DivAnimated element={ <Contact/> }/></Box>
         </div>
     )
 };
+
+const Box = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
