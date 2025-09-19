2a60c08940ea29df0dc750aa1f9532cfafec4ee3
pablosingh <pablo22_4@hotmail.com>
2025-09-18 22:14:40 -0300

    actualizacion de foto de perfil

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,39 +1,40 @@
-import image from '../img/foto_perfil.jpeg';
+// import image from '../img/foto_perfil.jpeg';
+import image from '../img/foto_cv_sep_25.jpg';
 import s from '../styles/Name.module.css';
 import styled from 'styled-components';
 
 export const Name = () => {
-    return (
-        // <div className={s.container} id='name'>
-        <Container id='name'>
-            <div className={s.card}>
-                <div className={s.imgDiv}>
-                    <img src={image} alt="Foto Perfil"/>
-                </div>
-                <Hache1>Pablo Singh</Hache1>
-                <Hache3>Pablo Singh</Hache3>
-            </div>
-        {/* </div> */}
-        </Container>
-    ) 
+  return (
+    // <div className={s.container} id='name'>
+    <Container id="name">
+      <div className={s.card}>
+        <div className={s.imgDiv}>
+          <img src={image} alt="Foto Perfil" />
+        </div>
+        <Hache1>Pablo Singh</Hache1>
+        <Hache3>Pablo Singh</Hache3>
+      </div>
+      {/* </div> */}
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    max-width: 100vw;
-    height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  max-width: 100vw;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Hache1 = styled.h1`
-    @media(max-width: 768px){
-        display: none;
-    }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Hache3 = styled.h3`
-    @media(min-width: 768px){
-        display: none;
-    }
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
